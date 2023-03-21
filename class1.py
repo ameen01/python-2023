b25a6e321ea5341889c85d887a4335971431f90d
ameen01 <pcameen@outlook.com>
2023-03-21 18:41:34 +0200

    class1

---
+++ class1.py
@@ -0,0 +1,17 @@
+x = 'asckftuyiomblpgjkxzbewmhkopjkxl'
+
+print()
+print()
+print()
+
+# def add(c,v):
+#     u = c+v
+#     return u
+
+# total =add(x,z)
+# print(total)
+
+# def sub(e,r):
+#     w = e-r
+#     return w
+
