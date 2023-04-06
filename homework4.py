0fce9a5ef23707e28ca6c01fa4027bebd28724ad
ameen01 <pcameen@outlook.com>
2023-04-06 16:45:26 +0300

    class4

---
+++ homework4.py
@@ -0,0 +1,17 @@
+# for more info!(conditions) --https://www.w3schools.com/python/python_conditions.asp
+# for more info!(operators) --https://www.w3schools.com/python/python_operators.asp
+# for more info!(string) --https://www.w3schools.com/python/python_strings_methods.asp
+
+#1 Write a program that takes two integers as input and prints the larger of the two.
+
+#2 Write a program that takes a string as input and prints the length of the string if it is greater than 10, and "Length less than or equal to 10" otherwise
+
+#3 Write a program that takes an integer as input and prints "Even" if the integer is even and "Odd" if it is odd.
+
+#4 Write a program that takes an integer as input and prints "Positive" if it is positive, "Negative" if it is negative, and "Zero" if it is zero.
+
+#5 Write a program that takes a string as input and checks if it starts with the letter "A". If it does, print "Starts with A", otherwise print "Does not start with A".
+
+#6 Write a program that takes two strings as input and prints "Equal" if they are equal, and "Not equal" otherwise.
+
+#7 Write a program that takes a string as input and prints "Uppercase" if all the letters in the string are uppercase, and "Not uppercase" otherwise.
