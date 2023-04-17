34698b4b3b0fcb516b31190573889b53f3f1106c
ameen01 <pcameen@outlook.com>
2023-04-17 22:40:04 +0300

    17/4

--- homework4.py
+++ homework4.py
@@ -3,10 +3,39 @@
 # for more info!(string) --https://www.w3schools.com/python/python_strings_methods.asp
 
 #1 Write a program that takes two integers as input and prints the larger of the two.
+# num1 = input(' nubmer 1:')
+# num2 = input(' number 2:')
+# num1 = int(num1)
+# num2 = int(num2)
 
-#2 Write a program that takes a string as input and prints the length of the string if it is greater than 10, and "Length less than or equal to 10" otherwise
+# if num1 > num2:
+#     print(num1, 'is larger')
+# if num2 > num1:
+#     print(num2, 'is larger')
+
+
+
+#2 Write a program that takes a string as input and prints the length of the string if it is greater than 10, and "Length less than or equal to 10" 
+
+# phrases = input(' type your word:')
+# max = 10
+# if len(phrases) > max:
+#     print(phrases,'is',len(phrases))
+# if len(phrases) < max:
+#     print(phrases,'is',len(phrases))
+# if len(phrases) == max:
+#     print(phrases,'is',len(phrases))
+
+
 
 #3 Write a program that takes an integer as input and prints "Even" if the integer is even and "Odd" if it is odd.
+# usr_input = int(input('tybe your number:'))
+# if (usr_input % 2) == 0:
+#     print('even')
+# else:
+#     print('odd')
+# print(8%2)
+# print(9%2)
 
 #4 Write a program that takes an integer as input and prints "Positive" if it is positive, "Negative" if it is negative, and "Zero" if it is zero.
 
@@ -15,3 +44,19 @@
 #6 Write a program that takes two strings as input and prints "Equal" if they are equal, and "Not equal" otherwise.
 
 #7 Write a program that takes a string as input and prints "Uppercase" if all the letters in the string are uppercase, and "Not uppercase" otherwise.
+
+
+user_input = input('type your  word:')
+
+if user_input.startswith('A'):
+    print('Start with A')
+else:
+    print('Does not start with A')
+
+# x= 'food'
+# print(x[3])
+# print(x[2])
+# print(x[-1])
+
+strin1= input()
+strin2= input()
--- image.py
+++ image.py
@@ -1,7 +1,2 @@
 import os
 import openai
-
-ke= "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"
-
-openai.api_key = ke
-
