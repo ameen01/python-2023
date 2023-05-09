8757962e15832274c16e74af37980d0a96ae0665
ameen01 <pcameen@outlook.com>
2023-05-09 18:21:05 +0300

    class7

---
+++ class7.py
@@ -0,0 +1,42 @@
+# my_list = []
+
+# for x in range(7):
+#     my_list.append(x)
+# print(my_list)
+
+# new_list = [1,2,3,4,5,6,7]
+# print(new_list)
+
+# for number in range(4):
+#     new_list.append(number)
+# print(new_list)
+
+# my_name = 'ameen'
+# for x in my_name:
+#     print(x.upper())
+
+# numbers = [1,10,3,4,5,7,100,2,20,22]
+# new_numbers = []
+
+# for number in numbers:
+#     if number % 2 ==0:
+#         new_numbers.append(number)
+
+# print(new_numbers)
+
+
+# my_list =  ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+# word = 'mango'
+
+# for x in my_list:
+#     if x == word:
+#         print('yes')
+#     else:
+#         x != word
+#         print('no')
+m = []
+for x in range(1,100):
+    m.append(x)
+
+print(m)
