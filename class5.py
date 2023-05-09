2bdb6fe187541ee023b3270bff08bfcf83e9b491
ameen01 <pcameen@outlook.com>
2023-05-09 16:18:00 +0300

    calss 7

---
+++ class5.py
@@ -0,0 +1,62 @@
+# Write a program that tell you how old are you today?
+
+day = int(input('day:'))
+
+month = int(input('month:'))
+
+this_year= 2023
+
+year = int(input('year:'))
+print('you are ',this_year- year,' years old')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# how_old = [31, 10, 2023]
+# dd = input("day:>")
+# mm = input("month:>")
+# yy = input("year:>")
+# yy = int(yy)
+# how_old[0] = dd
+# how_old[1] = mm
+# year = how_old[2]-yy
+# print(year)
+
+
---
+++ class6.py
@@ -0,0 +1,50 @@
+#https://www.w3schools.com/python/python_lists_methods.asp
+
+# print(int())
+# print(str())
+# print(float())
+# print(bool())
+#---------------------
+# x = [122,3,4,5,6,7,8,9,10]
+# add the number 11
+#--------------------
+# remove the number 5
+#--------------------
+# how may item in th list
+# print(x)
+
+#my_list =  ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+#my_list2 = ['cat',1,'dog',43,'s',1.435, True]
+
+#new_list =[1,5,97,3,0,6]
+
+#ameen = []
+
+#new_list.append(my_list[1])
+#my_list.sort()
+#print(my_list)
+
+
+# Write a program to reverse a list.
+
+# my_list =  ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# my_list.reverse()
+# print(my_list)
+
+# #_----------------------------_
+# # Write a program to check if a list is empty or not.
+
+# if len(my_list) == 0 :
+#     print('the list is empty')
+# if len(my_list) > 0 :
+#     print('the list not empty')
+
+# write a program to  check if two list are the same size
+
+my_list =  ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+my_list2 =  ["cherry", "orange", "kiwi", "melon", "mango"]
+
+if len(my_list) == len(my_list2):
+    print('the list same size')
+if len(my_list) != len(my_list2):
+    print('the list are not same size')
