3365ecbcf6b5852a3fcbff44cc37c334085e6ca6
ameen01 <pcameen@outlook.com>
2023-05-14 17:18:37 +0300

    calss work

---
+++ class7_class_work.py
@@ -0,0 +1,23 @@
+# Write a program that prints the numbers from 1 to 1000 using a for loop.
+# for x in range(1,1000):
+#     print(x)
+#Write a program that takes a list of numbers and prints the square of each number using a for loop.
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# for num in numbers:
+#     print(num**2)
+
+#Write a program that takes a string and prints each character in the string using a for loop.
+# word = 'python'
+# for x in word:
+#     print(x)
+
+#Write a program that takes a list of words and prints the length of each word using a for loop.
+#Write a program that prints the sum of the numbers from 1 to 100 using a for loop.
+# my_list = [1,2,3,'food',1.33,True,[21]]
+# x= 'ameen','adam'
+# y = '3'
+# print(list(x) )
+# print(int(y) )
+# #-----------------------------------
+# for x in my_list:
+#     print(x)
