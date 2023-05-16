2591f34da23442b42779844650284be1286eb81f
ameen01 <pcameen@outlook.com>
2023-05-16 18:00:33 +0300

    homwork

--- class 8.py
+++ class 8.py
@@ -1,8 +1,22 @@
-#You have been given a list of names. Your task is to iterate over the list and print only the names that start with the letter 'A'.
-#Write a program that takes a list of numbers as input and calculates the sum of all the positive numbers in the list. The program should then print the sum.
-#Write a program that takes a list of strings as input, and outputs the length of the longest string in the list.
+#----------------------------------------------------------
+
+#You have been given a list of names. Your task is to iterate over
+#the list and print only the names that start with the letter 'A'.
 
 names = ['hadil','hamodi','lara', 'ameen','mayada','rian']
-#---------------------------------------------------------
 
+#1)
+for name in names:
+    if name.startswith('a'):
+        print(name ,'start with a ')
+
+#2)
+for name in names:
+    if name[0] == 'a':
+        print(name ,'start with a too')
+        
+    
 #----------------------------------------------------------
+
+#Write a program that takes a list of numbers as input and calculates the sum of all the positive numbers in the list. The program should then print the sum.
+#Write a program that takes a list of strings as input, and outputs the length of the longest string in the list.
